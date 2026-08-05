1eacc796f33205f8bec62357878b6b2adc118018
code-Harsh247 <justharsh2407@gmail.com>
2026-08-06 00:56:50 +0530

    Sliding Window Sum

---
+++ Sliding Window Problems/SlidingWindowSum.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1000000000
+#define MOD 1000000007
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll,ll> pll;
+
+void generateInput(vll&input, ll x, ll a, ll b, ll c,ll n){
+    input[0] = x;
+    for(ll i=1;i<n;i++){
+        input[i] = (a*input[i-1]+b)%c;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n,k;
+    cin>>n>>k;
+    ll x,a,b,c;
+    cin>>x>>a>>b>>c;
+
+    vll input(n);
+    generateInput(input, x,a,b,c,n);
+    ll l=0,r=0;
+    ll sum=0;
+    ll count=0;
+    ll ans=0;
+    while(r<n){
+        sum+=input[r];
+        count++;
+        while(count>k){
+            sum-=input[l];
+            count--;
+            l++;
+        }
+        if(count == k) ans = ans^sum;
+        r++;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
