5b193ec1a9f19dc669572afd7cfc448a619ff183
code-Harsh247 <justharsh2407@gmail.com>
2026-08-23 02:24:08 +0530

    Removal Game

--- Dynamic Programming/RemovalGame.cpp
+++ Dynamic Programming/RemovalGame.cpp
@@ -1,26 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define INF 1000000000
+#define MOD 1000000007
+
 typedef long long ll;
+typedef vector<ll> vll;
+typedef pair<ll,ll> pll;
 
-ll solve(vector<ll>& list, vector<vector<ll>>& dp, ll i, ll j) {
-    if (i > j) return 0;
-    if (dp[i][j] != -1) return dp[i][j];
+ll solve(vector<ll>&list,ll l, ll r, vector<vector<ll>>&dp){
+    if(l>r) return 0;
+    if(dp[l][r]!=-1) return dp[l][r];
 
-    ll pickLeft = list[i] + min(solve(list, dp, i + 2, j), solve(list, dp, i + 1, j - 1));
-    ll pickRight = list[j] + min(solve(list, dp, i + 1, j - 1), solve(list, dp, i, j - 2));
+    ll left = list[l] - solve(list,l+1,r, dp);
+    ll right = list[r] - solve(list,l,r-1, dp);
 
-    return dp[i][j] = max(pickLeft, pickRight);
+    return dp[l][r] = max(left, right);
 }
 
-int main() {
-    ll n;
-    cin >> n;
-    vector<ll> list(n);
-    for (ll i = 0; i < n; i++) cin >> list[i];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    vector<vector<ll>> dp(n, vector<ll>(n, -1));
-    cout << solve(list, dp, 0, n - 1) << endl;
+    ll n;
+    cin>>n;
+    vector<ll> list(n);
+    for(ll i=0;i<n;i++){
+        cin>>list[i];
+    }
+    vector<vector<ll>> dp(n, vector<ll>(n,-1));
+    ll total = accumulate(list.begin(), list.end(), 0LL);
+    ll maxDiff = solve(list,0,n-1,dp);
+    ll maxScore = (total + maxDiff)/2;
+    cout<<maxScore<<endl;
+
 
     return 0;
 }
