5b29a1e2bb640ceac74975e53981560241545cbe
code-Harsh247 <justharsh2407@gmail.com>
2026-09-14 16:11:57 +0530

    RangeXORQueries

---
+++ Range Queries/RangeXORQueries.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define INF 1000000000
+#define MOD 1000000007
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<ll> vi;
+typedef pair<ll,ll> pll;
+typedef pair<ll,ll> pii;
+
+class SegmentTree{
+    ll n;
+    vector<ll> tree;
+    public :
+    SegmentTree(ll size){
+        n = size;
+        tree.resize(2*n);
+    }
+    void update(ll p, ll val){
+        for(tree[p+=n]=val;p>1;p>>=1){
+            tree[p>>1] = tree[p&~1] ^ tree[p|1];
+        }
+    }
+    ll query(ll l, ll r){
+        ll res = 0;
+        for(l+=n,r+=n+1;l<r;l>>=1,r>>=1){
+            if(l&1) res ^= tree[l++];
+            if(r&1) res ^= tree[--r];
+        }
+        return res;
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n,q;
+    cin>>n>>q;
+    SegmentTree st(n);
+    for(ll i=0;i<n;i++){
+        ll num;
+        cin>>num;
+        st.update(i,num);
+    }
+    for(ll i=0;i<q;i++){
+        int a,b;
+        cin>>a>>b;
+        a--;
+        b--;
+        cout<<st.query(a,b)<<endl;
+    }
+
+    return 0;
+}
